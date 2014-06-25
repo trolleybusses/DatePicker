385107ed8a108a479f04d8bd453d26d8102619a9
Matt Pearce <mpearce@sorensonmedia.com>
2014-06-25 11:47:07 +0100

    Changed getWeekNumber function on Date object to return proper week numbers.

--- js/datepicker.js
+++ js/datepicker.js
@@ -489,10 +489,8 @@
           return d - 1;
         };
         Date.prototype.getWeekNumber = function(){
-            var d = new Date(+this);
-            d.setHours(0,0,0);
-            d.setDate(d.getDate()+4-(d.getDay()||7));
-            return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+          var onejan = new Date(this.getFullYear(), 0, 1);
+          return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
         };
       },
       
